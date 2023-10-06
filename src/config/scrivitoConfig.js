053f5ac5701bc6f7d846abcba2e44447450a9ced
Amier El Ahmad <amier.elahmad@justrelate.com>
2023-10-06 13:05:46 +0200

    initial commit

---
+++ src/config/scrivitoConfig.js
@@ -0,0 +1,14 @@
+let tenant = null;
+let neoletterSubscriptionFeatureEnabled = null
+
+export const setScrivitoFormWidgetConfig = (scrivitoTenant, neoletterFormSubmissionFeatureEnabled) => {
+    tenant = scrivitoTenant;
+    neoletterSubscriptionFeatureEnabled = neoletterFormSubmissionFeatureEnabled
+}
+
+export const getScrivitoFormWidgetConfig = () => {
+    return {
+        tenant: tenant,
+        neoletterSubmissionEnabled: neoletterSubscriptionFeatureEnabled
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import  "./Widgets/FormCheckboxWidget/FormCheckboxWidgetComponent"
+import  "./Widgets/FormCheckboxWidget/FormCheckboxWidgetEditingConfig"
+import "./Widgets/FormCheckboxWidget/FormCheckboxWidgetClass"
+
+import "./Widgets/FormConditionWidget/FormConditionWidgetClass"
+import "./Widgets/FormConditionWidget/FormConditionWidgetComponent"
+import "./Widgets/FormConditionWidget/FormConditionWidgetEditingConfig"
+// import "./FormConditionWidget/FormConditionWidget.scss"
+
+import "./Widgets/FormConditionalContainerWidget/FormConditionalContainerWidgetClass"
+import "./Widgets/FormConditionalContainerWidget/FormConditionalContainerWidgetComponent"
+import "./Widgets/FormConditionalContainerWidget/FormConditionalContainerWidgetEditingConfig"
+// import "./FormConditionalContainerWidget/FormConditionalContainerWidget.scss"
+
+import "./Widgets/FormContainerWidget/FormContainerWidgetClass"
+import "./Widgets/FormContainerWidget/FormContainerWidgetComponent"
+import "./Widgets/FormContainerWidget/FormContainerWidgetEditingConfig"
+// import "./FormContainerWidget/FormContainerWidget.scss"
+
+import "./Widgets/FormDateWidget/FormDateWidgetClass"
+import "./Widgets/FormDateWidget/FormDateWidgetComponent"
+import "./Widgets/FormDateWidget/FormDateWidgetEditingConfig"
+// import "./FormDateWidget/FormDateWidget.scss"
+
+import "./Widgets/FormHiddenFieldWidget/FormHiddenFieldWidgetClass"
+import "./Widgets/FormHiddenFieldWidget/FormHiddenFieldWidgetComponent"
+import "./Widgets/FormHiddenFieldWidget/FormHiddenFieldWidgetEditingConfig"
+
+import "./Widgets/FormInputFieldWidget/FormInputFieldWidgetClass"
+import "./Widgets/FormInputFieldWidget/FormInputFieldWidgetComponent"
+import "./Widgets/FormInputFieldWidget/FormInputFieldWidgetEditingConfig"
+
+import "./Widgets/FormRatingWidget/FormRatingWidgetClass"
+import "./Widgets/FormRatingWidget/FormRatingWidgetComponent"
+import "./Widgets/FormRatingWidget/FormRatingWidgetEditingConfig"
+// import "./FormRatingWidget/FormRatingWidget.scss"
+
+import "./Widgets/FormSelectWidget/FormSelectWidgetClass"
+import "./Widgets/FormSelectWidget/FormSelectWidgetComponent"
+import "./Widgets/FormSelectWidget/FormSelectWidgetEditingConfig"
+// import "./FormSelectWidget/FormSelectWidget.scss"
+
+import "./Widgets/FormStepWidget/FormStepWidgetClass"
+import "./Widgets/FormStepWidget/FormStepWidgetComponent"
+import "./Widgets/FormStepWidget/FormStepWidgetEditingConfig"
+// import "./FormStepWidget/FormStepWidget.scss"
+
+import {setScrivitoFormWidgetConfig} from  "./config/scrivitoConfig";
+
+ export {setScrivitoFormWidgetConfig} 
